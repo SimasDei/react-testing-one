2f3c3dfbc39c3770684f619d7e57445a23549294
SimasDei <simondeiman@gmail.com>
2019-01-03 11:04:10 +0200

    testing testing 123

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
 
-it('renders without crashing', () => {
+it('shows comment box', () => {
   const div = document.createElement('div');
+
   ReactDOM.render(<App />, div);
+
+  // Checks for CommentBox
+  expect(div.innerHTML).toContain('Comment Box');
+
   ReactDOM.unmountComponentAtNode(div);
 });
